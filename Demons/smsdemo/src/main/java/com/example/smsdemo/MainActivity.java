fb95b0e225bbd3d06a96a802bb7ab1a44a960c62
portguas <answerhyl@126.com>
2016-06-22 17:51:56 +0800

    add

--- Demons/smsdemo/src/main/java/com/example/smsdemo/MainActivity.java
+++ Demons/smsdemo/src/main/java/com/example/smsdemo/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.smsdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
+import cn.smssdk.gui.RegisterPage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SMSSDK.initSDK(this, "142be1af3890c", "2350464fb38395b89c5ce6871f6c7c0b");
+
+        initSmsView();
+    }
+
+    private void initSmsView() {
+        //打开注册页面
+        RegisterPage registerPage = new RegisterPage();
+        registerPage.setRegisterCallback(new EventHandler() {
+            public void afterEvent(int event, int result, Object data) {
+// 解析注册结果
+                if (result == SMSSDK.RESULT_COMPLETE) {
+                    @SuppressWarnings("unchecked")
+                    HashMap<String,Object> phoneMap = (HashMap<String, Object>) data;
+                    String country = (String) phoneMap.get("country");
+                    String phone = (String) phoneMap.get("phone");
+
+// 提交用户信息
+//                    registerUser(country, phone);
+                }
+                if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
+                    Toast.makeText(MainActivity.this, "submit", Toast.LENGTH_SHORT).show();
+
+                } else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
+                    Toast.makeText(MainActivity.this, "verification", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        registerPage.show(MainActivity.this);
     }
 }
